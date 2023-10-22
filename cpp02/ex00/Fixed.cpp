fa5770f5d454e6768bae8af68bd84b30e8e3c97d
KarinaLogvina <karinalogvina0@gmail.com>
2023-10-22 14:31:46 +0200

    fixed ex 00

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: Tiiksii <Tiiksii@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:09:44 by Tiiksii           #+#    #+#             */
-/*   Updated: 2023/09/29 17:09:45 by Tiiksii          ###   ########.fr       */
+/*   Updated: 2023/10/22 14:30:16 by Tiiksii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ Fixed::Fixed() : _rawBits(0) {
 };
 
 Fixed::Fixed(Fixed const & other) {
+    std::cout << "Copy constructor called" << std::endl;
     *this = other;
 }
 
@@ -28,17 +29,19 @@ Fixed::~Fixed() {
 Fixed & Fixed::operator=(const Fixed & rhs) {
     if (this != & rhs)
     {
-        std::cout << "Assignation operator called" << std::endl;
-        this->setRawBits(rhs.getRawBits());
+        std::cout << "Copy assignment operator called" << std::endl;
+        this->_rawBits = rhs.getRawBits();
     }
     return *this;
 };
 
 int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
     return (this->_rawBits);
 };
 
 void Fixed::setRawBits (int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
     this->_rawBits = raw;
 	return;
 }
